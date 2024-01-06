6521148ecda525e164c3e2afa050dea26f94e1ac
Rendell Peh <rendell.email@gmail.com>
2024-01-06 15:57:04 +0800

    ✅ generate 12 months header

--- calendar.js
+++ calendar.js
@@ -18,17 +18,22 @@ function generateMain() {
 
     // TODO: 
     // ✅ generate the table
-    // - generate 12 months header
+    // ✅ generate 12 months header
     // - generate individual days under each month 
 
     let calendarTable = document.createElement('table');
     let tableHeader = document.createElement('thead');
     let tableBody = document.createElement('tbody');
-    
-    tableHeader.innerHTML = '<tr><th>Calendar Table Header</th><tr>'
-    tableBody.innerHTML = '<tr><td>Calendar Table Body</td><tr>'
+
+    tableHeader.innerHTML = generateHeadings();
+    tableBody.innerHTML = '<tr><td>Calendar Table Body</td></tr>'
 
     calendarTable.appendChild(tableHeader);
     calendarTable.appendChild(tableBody);
     calendarMain.appendChild(calendarTable);
+}
+
+function generateHeadings() {
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    return '<tr>' + months.map(month => `<th>${month}</th>`).join('') + '</tr>';
 }
