a1aaefe1d39acff74eb61e125d1d192eeed463fb
Rendell Peh <rendell.email@gmail.com>
2024-01-06 16:33:07 +0800

    ✅ generate individual days under each month

--- calendar.js
+++ calendar.js
@@ -19,13 +19,11 @@ function generateMain() {
     // TODO: 
     // ✅ generate the table
     // ✅ generate 12 months header
-    // - generate individual days under each month 
+    // ✅ generate individual days under each month 
 
     let calendarTable = document.createElement('table');
-    let tableBody = document.createElement('tbody');
-
-    tableBody.innerHTML = '<tr><td>Calendar Table Body</td></tr>'
     let tableHeader = generateHeadings();
+    let tableBody = generateDays();
 
     calendarTable.appendChild(tableHeader);
     calendarTable.appendChild(tableBody);
@@ -42,4 +40,26 @@ function generateHeadings() {
     tableHeader.innerHTML = '<tr>' + months.map(month => `<th>${month}</th>`).join('') + '</tr>';
 
     return tableHeader;
+}
+
+function generateDays() {
+    // - generate tbody
+    // - generate 31 rows 
+    // - generate 12 day cells in each row
+    // - determine last day of month and stop adding the day cell
+    // - return tbody
+
+    let tableBody = document.createElement('tbody');
+    for (let day = 1; day <= 31; day++) {
+        let row = document.createElement('tr');
+        for (let month = 1; month <= 12; month++) {
+            let dayCell = document.createElement('td');
+            dayCell.textContent = day;
+            row.appendChild(dayCell)
+        }
+        tableBody.appendChild(row);
+    }
+
+    return tableBody;
+
 }
