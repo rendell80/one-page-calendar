7822dea61cdb1ae19b8750718d4cce9b58f5bfaa
Rendell Peh <rendell.email@gmail.com>
2024-01-06 15:14:46 +0800

    clean up - break into functions

--- calendar.js
+++ calendar.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
+    generateTitle();
+    generateMain();
+});
+
+function generateTitle() {
     const calendarTitle = document.getElementById('calendar-title');
-    const calendarMain = document.getElementById('calendar-table');
 
     // TODO: 
     // - get and print current year
 
     calendarTitle.textContent = "One Page Calendar - 2024";
+}
+
+function generateMain() {
+    const calendarMain = document.getElementById('calendar-table');
 
     // TODO: 
     // - generate the table
@@ -13,5 +21,4 @@ document.addEventListener('DOMContentLoaded', function() {
     // - generate individual days under each month 
 
     calendarMain.textContent = "Main Calendar Table"
-
-});
+}
