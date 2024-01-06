838c98fac890247bc975826502fe07094201179a
Rendell Peh <rendell.email@gmail.com>
2024-01-06 15:31:26 +0800

    ✅ generate calendar table

--- calendar.js
+++ calendar.js
@@ -10,16 +10,25 @@ function generateTitle() {
     // TODO: 
     // ✅ get and print current year
 
-    calendarTitle.textContent = "One Page Calendar - " + year;
+    calendarTitle.textContent = 'One Page Calendar - ' + year;
 }
 
 function generateMain() {
     const calendarMain = document.getElementById('calendar-table');
 
     // TODO: 
-    // - generate the table
+    // ✅ generate the table
     // - generate 12 months header
     // - generate individual days under each month 
 
-    calendarMain.textContent = "Main Calendar Table"
+    let calendarTable = document.createElement('table');
+    let tableHeader = document.createElement('thead');
+    let tableBody = document.createElement('tbody');
+    
+    tableHeader.innerHTML = '<tr><th>Calendar Table Header</th><tr>'
+    tableBody.innerHTML = '<tr><td>Calendar Table Body</td><tr>'
+
+    calendarTable.appendChild(tableHeader);
+    calendarTable.appendChild(tableBody);
+    calendarMain.appendChild(calendarTable);
 }
