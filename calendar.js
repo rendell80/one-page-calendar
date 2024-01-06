fc212480367d4edfc719c7543ffb37b67141d2c1
Rendell Peh <rendell.email@gmail.com>
2024-01-06 15:10:04 +0800

    add TODOs

--- calendar.js
+++ calendar.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const calendarTitle = document.getElementById('calendar-title')
-    calendarTitle.textContent = "2024";
+    const calendarTitle = document.getElementById('calendar-title');
+    const calendarMain = document.getElementById('calendar-table');
+
+    // TODO: 
+    // - get and print current year
+
+    calendarTitle.textContent = "One Page Calendar - 2024";
+
+    // TODO: 
+    // - generate the table
+    // - generate 12 months header
+    // - generate individual days under each month 
+
+    calendarMain.textContent = "Main Calendar Table"
+
 });
